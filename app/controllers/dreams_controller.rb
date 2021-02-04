b13ef60e80ae8c79a356dfaa94f6d7f1790dd412
Breanna <breanna@breannawebster.com>
2021-02-04 13:36:59 -0500

    Dreams CRUD

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,20 +1,50 @@
 class DreamsController < ApplicationController
-  def index
-  end
 
-  def show
+
+  def index
+    @dreams = Dream.all
   end
 
   def new
+    @dream = Dream.new
+  end
+
+  def show
+    @dream = Dream.findby(id params[:id])
   end
 
   def create
+    @dream = Dream.new(dreams_params)
+    if @dream.save
+      redirect_to @dream
+    else
+      render :new
+    end
   end
 
   def edit
+    @dream = Dream.find(params[:id])
   end
 
   def update
+    @dream = Dream.find(params[:id])
+
+    if @dream.update(dreams_params)
+      redirect_to @dream
+    else
+      render :edit
+    end
+  end
+
+  def destoy
+    @dream = Dream.find(params[:id])
+    @dream.destroy
+  end
+
+  private
+
+  def dreams_params
+    params.require(:dream).permit (:name, :date, :dream, :reflections, :dreamjournals_id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :dreams
+  resources :dreams #7 RESTful routes
   resources :dream_journals
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
