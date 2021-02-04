a99f84778f26580176f5c067271afcb0cb90fae9
Breanna <breanna@breannawebster.com>
2021-02-04 14:06:37 -0500

    User Controller setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,41 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.findby(id params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(dreams_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit (:name, :email, :password)
+  end
+  
 end
