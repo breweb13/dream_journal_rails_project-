204467bffa3e80b73bc0523155ff574440bba86a
Breanna <breanna@breannawebster.com>
2021-02-06 00:38:39 -0500

    update relationships

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,4 +1,3 @@
 class Dream < ApplicationRecord
   belongs_to :dream_journal
-  has_many :users, through: :dream_journals
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_many :dream_journals
   has_many :dreams, through: :dream_journal
+  has_many :feelings, through: :dream_journal
+
+  has_secure_password
 end
