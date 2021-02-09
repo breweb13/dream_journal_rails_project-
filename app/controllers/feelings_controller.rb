241578c29573875dd6606263348f01185a9e38c1
Breanna <breanna@breannawebster.com>
2021-02-09 12:00:36 -0500

    Crud Feeling Controllers

--- app/controllers/feelings_controller.rb
+++ app/controllers/feelings_controller.rb
@@ -1,2 +1,52 @@
 class FeelingsController < ApplicationController
+  
+  def index
+    @feelings = Feeling.all
+    
+  end
+
+  def new
+    @feeling = Feeling.new
+  end
+
+  def show
+  end
+
+  def create
+    @feeling = Feeling.new(feeling_params)
+    if @feeling.save
+      redirect_to feelings_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    find__feeling
+  end
+
+  def update
+    find_feeling
+    @feeling.update(feelings_params)
+    if @feeling.valid?
+      redirect_to feelings_path
+    else
+      render :edit
+    end
+  end
+
+  def destoy
+    find_dream
+    @feeling.destroy
+    redirect_to feelings_path
+  end
+
+  private
+
+  def feelings_params
+    params.require(:feeling).permit(:type, :description)
+  end
+
+  def find_feeling
+    @feeling = Feeling.find(params[:id])
 end
