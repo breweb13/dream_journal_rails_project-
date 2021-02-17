f6316a6965e2d1d5eae970965a306a0b9ca48484
Breanna <breanna@breannawebster.com>
2021-02-17 18:08:44 -0500

    Refactoring Before_Action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,15 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
+ 
   def current_user
     @current_user ||=User.find(session[:user_id]) if session[:user_id]
   end
+  
   helper_method :current_user
+  
   private
 
-  def redirect_if_not_logged_in
-      redirect_to '/login' if !logged_in?
-  end
-
-  def redirect_if_logged_in
-      redirect_to user_path(current_user) if logged_in?
-  end
+ 
  
 
 end
